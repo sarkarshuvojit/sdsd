b74d6a75301a0f8bcdf6ac6441beccd704898150
Shuvojit <s15sarkar@yahoo.com>
2024-01-17 12:19:02 +0530

    reading gps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -19,17 +18,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        appBarTheme:const AppBarTheme(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(
             backgroundColor: Color(0xFF1B1464),
             foregroundColor: Colors.white,
-        )
-      ),
+          )),
       home: Scaffold(
         backgroundColor: const Color(0xFFecf0f1),
         appBar: AppBar(
-            title: const Text("Slow down, sound down"),
+          title: const Text("Slow down, sound down"),
         ),
         body: const SdSd(),
       ),
@@ -38,75 +36,127 @@ class MyApp extends StatelessWidget {
 }
 
 abstract class SdSdConfig {
-    // Volume when not moving
-    static const int idleVolume = 20; // volume %
+  // Volume when not moving
+  static const int idleVolume = 20; // volume %
 
-    // At what speed should 100% volume be used 
-    static const int fullVolumeSpeed = 40; // volume %
+  // At what speed should 100% volume be used
+  static const int fullVolumeSpeed = 40; // volume %
 
-    // Below what speed should the volume start going down
-    static const int slowDownThreshold = 15; // kmph
-
+  // Below what speed should the volume start going down
+  static const int slowDownThreshold = 15; // kmph
 }
 
 abstract class Messages {
-    
-    static const String permissionErrorSnackbar = "We cannot track your speed without location persmission.";
+  static const String permissionErrorSnackbar =
+      "We cannot track your speed without location persmission.";
 }
 
-enum MovementStates {
-    idle,
-    acceleration,
-    deceleration
-}
+enum MovementStates { idle, acceleration, deceleration }
 
 class SdSdController extends GetxController {
-    var speed = 0.obs;
-    var vol = 0.obs;
-    var curMovementState = MovementStates.idle;
-    var isLocationServiceDisabled = false.obs;
-    var isPermissionGranted = false.obs;
+  var speed = 0.obs;
+  var vol = 0.obs;
+  var curMovementState = MovementStates.idle;
+  var isLocationServiceDisabled = false.obs;
+  var isPermissionGranted = false.obs;
 
-    @override
-    void onInit() {
-        super.onInit();
-        startPolling();
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 50,
+  );
+
+  final AndroidSettings androidLocationSettings = AndroidSettings(
+    intervalDuration: const Duration(seconds: 3),
+    distanceFilter: 2,
+
+    forceLocationManager: true,
+  );
+
+  @override
+  void onInit() {
+    super.onInit();
+    startPolling();
+  }
+
+
+  int _mPerSecondToKmh(double speedInMps) {
+      // Perform the conversion using the correct factor for meters per second to kilometers per hour
+      double speedInKmh = speedInMps * 3.6;
+
+      // Round the result to the nearest integer using a more precise method
+      return (speedInKmh + 0.5).toInt();
+  }
+
+
+  void startPolling() async {
+    log("StartPolling");
+    var isPermissionGranted = await getPermission();
+
+    if (!isPermissionGranted) {
+      log("Permisssion Denied :(");
     }
 
-    void startPolling() async {
-        log("StartPolling");
-        var isPermissionGranted = await getPermission();
+    Geolocator
+        .getPositionStream(locationSettings: androidLocationSettings)
+        .listen((Position? position) {
+          if (position != null) {
+            log("Found a new position $position going at ${position.speed}");
+            var speedInMetersPerSec = position.speed;
+            var speedInKmPerHr = _mPerSecondToKmh(speedInMetersPerSec);
 
-        if (!isPermissionGranted) {
-            Get.snackbar("Oops!", Messages.permissionErrorSnackbar).show();
-        }
+            log("Current speed: $speedInKmPerHr km/h");
+            speed.value = speedInKmPerHr;
+          }
+        });
+  }
 
+  Future<bool> getPermission() async {
+    bool serviceEnabled;
+    LocationPermission locationPermission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      log("Location service is disabled");
+      isLocationServiceDisabled.value = true;
     }
 
-    Future<bool> getPermission() async {
-        bool serviceEnabled;
-        LocationPermission locationPermission;
+    locationPermission = await Geolocator.checkPermission();
+    log("Got location permission, $locationPermission");
 
-        serviceEnabled = await Geolocator.isLocationServiceEnabled();
-        if (!serviceEnabled) {
-            log("Location service is disabled");
-            isLocationServiceDisabled.value = true;
-        }
-
-        locationPermission = await Geolocator.checkPermission();
-        log("Got location permission, $locationPermission");
-        switch(locationPermission) {
-            case LocationPermission.always:
-            case LocationPermission.whileInUse:
-                log("Permission is granted");
-                 isPermissionGranted.value = true;
-                 isLocationServiceDisabled.value = false;
-                 return true;
-            default:
-                log("Permission is not granted");
-                return false;
-        }
+    if (locationPermission == LocationPermission.denied) {
+      locationPermission = await Geolocator.requestPermission();
+      if (locationPermission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        log('Location permissions are denied');
+        return false;
+      }
     }
+
+    if (locationPermission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      log('Location permissions are permanently denied, we cannot request permissions.');
+      return false;
+    }
+
+    // switch (locationPermission) {
+    //   case LocationPermission.always:
+    //   case LocationPermission.whileInUse:
+    //     log("Permission is granted");
+    //     isPermissionGranted.value = true;
+    //     isLocationServiceDisabled.value = false;
+    //     return true;
+    //   case LocationPermission.denied:
+    //     return
+    //   default:
+    //     log("Permission is not granted");
+    //     return false;
+    // }
+    return true;
+  }
 }
 
 class SdSd extends StatelessWidget {
@@ -117,16 +167,14 @@ class SdSd extends StatelessWidget {
     final SdSdController c = Get.put(SdSdController());
 
     return Center(
-        child: Wrap(
-            alignment: WrapAlignment.center,
-            direction: Axis.vertical,
-            children: [
-                Obx(() => Text("Current Speed: ${c.speed} km/h")),
-                Obx(() => Text("Current Volume: ${c.vol}%")),
-            ],
-        ),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        direction: Axis.vertical,
+        children: [
+          Obx(() => Text("Current Speed: ${c.speed} km/h")),
+          Obx(() => Text("Current Volume: ${c.vol}%")),
+        ],
+      ),
     );
   }
-
 }
-
